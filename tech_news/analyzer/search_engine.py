8e8f99810c6c9a0229bf063abaa4fa1e210fcc52
Ruan Miguel <ruanmiguelps@gmail.com>
2021-09-05 13:03:28 -0300

    Req 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -52,9 +52,18 @@ def search_by_source(source):
         return response_tuple
 
 
-print(search_by_source("ResetEra"))
-
-
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+    response = search_news(
+        {"categories": {"$regex": category, "$options": "i"}}
+    )
+    response_tuple = []
+    if len(response) == 0:
+        return []
+    else:
+        for title in response:
+            tuple_value = [title["title"], title["url"]]
+            respose_data = tuple(tuple_value)
+            response_tuple.append(respose_data)
+        return response_tuple
